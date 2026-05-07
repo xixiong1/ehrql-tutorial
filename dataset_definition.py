1f80b1e58244aa37418ceb5c80bc19fc8335e725
xixiong1 <lshxx1@lshtm.ac.uk>
2026-05-07 14:23:21 +0000

    update cohort and my quiz answer

--- dataset_definition.py
+++ dataset_definition.py
@@ -1,4 +1,75 @@
-from ehrql import show
+from ehrql import create_dataset, codelist_from_csv, show, days
 from ehrql.tables.core import patients, practice_registrations, clinical_events, medications
 
-show(patients)
+index_date = "2024-03-31"
+
+proteinuria_codes = codelist_from_csv("codelists/nhsd-primary-care-domain-refsets-prt_cod.csv", column="code")
+microalbuminuria_codes = codelist_from_csv("codelists/nhsd-primary-care-domain-refsets-mal_cod.csv", column="code")
+ace_codes = codelist_from_csv("codelists/opensafely-ace-inhibitor-medications.csv", column="code")
+arb_codes = codelist_from_csv("codelists/opensafely-angiotensin-ii-receptor-blockers-arbs.csv", column="code")
+diabetes_codes = codelist_from_csv("codelists/nhsd-primary-care-domain-refsets-dm_cod.csv", column="code")
+resolved_codes = codelist_from_csv("codelists/nhsd-primary-care-domain-refsets-dmres_cod.csv", column="code")
+
+previous_events = clinical_events.where(clinical_events.date.is_on_or_before(index_date))
+recent_meds = medications.where(medications.date.is_on_or_between(index_date - days(180), index_date))
+
+aged_17_or_older = patients.age_on(index_date) >= 17
+is_alive = patients.is_alive_on(index_date)
+is_registered = (
+    practice_registrations
+    .where(practice_registrations.start_date <= index_date)
+    .except_where(practice_registrations.end_date < index_date)
+    .exists_for_patient()
+)
+
+last_diagnosis_date = (
+    clinical_events.where(clinical_events.snomedct_code.is_in(diabetes_codes))
+    .sort_by(clinical_events.date)
+    .where(clinical_events.date <= index_date)
+    .last_for_patient()
+    .date
+)
+
+last_resolved_date = (
+    clinical_events.where(clinical_events.snomedct_code.is_in(resolved_codes))
+    .sort_by(clinical_events.date)
+    .where(clinical_events.date <= index_date)
+    .last_for_patient()
+    .date
+)
+
+has_unresolved_diabetes = last_diagnosis_date.is_not_null() & (last_resolved_date.is_null() | (last_resolved_date < last_diagnosis_date))
+
+on_register = aged_17_or_older & is_alive & is_registered & has_unresolved_diabetes
+
+has_proteinuria_diagnosis = (
+    previous_events
+    .where(clinical_events.snomedct_code.is_in(proteinuria_codes))
+    .exists_for_patient()
+)
+
+has_microalbuminuria_diagnosis = (
+    previous_events
+    .where(clinical_events.snomedct_code.is_in(microalbuminuria_codes))
+    .exists_for_patient()
+)
+
+has_arb_treatment = (
+    recent_meds
+    .where(medications.dmd_code.is_in(arb_codes))
+    .exists_for_patient()
+)
+
+has_ace_treatment = (
+    recent_meds
+    .where(medications.dmd_code.is_in(ace_codes))
+    .exists_for_patient()
+)
+
+dataset = create_dataset()
+dataset.define_population(on_register)
+
+dataset.prt_or_mal = has_proteinuria_diagnosis | has_microalbuminuria_diagnosis
+dataset.ace_or_arb = has_arb_treatment | has_ace_treatment
+
+show(dataset)
--- quiz.py
+++ quiz.py
@@ -2,8 +2,8 @@
 
 from quiz_answers import questions
 
-from ehrql import codelist_from_csv, show, months
-from ehrql.tables.core import clinical_events
+from ehrql import create_dataset, codelist_from_csv, show, months
+from ehrql.tables.core import clinical_events, patients
 
 
 diabetes_codes = codelist_from_csv(
@@ -34,46 +34,85 @@ questions[0].check(
 
 # Question 1
 # Create a patient series containing the date of each patient's earliest diabetes diagnosis.
-questions[1].check(...)
+questions[1].check(
+    clinical_events.where(clinical_events.snomedct_code.is_in(diabetes_codes))
+    .sort_by(clinical_events.date)
+    .first_for_patient()
+    .date
+)
 # If you need a hint for this, or any other, question, just uncomment (remove the #) from the following line:
 # questions[1].hint()
 
 # Question 2
 # Create a patient series containing the date of each patient's earliest structured education
 # programme referral. (Use the referral_code codelist.)
-questions[2].check(...)
+early_referral = (
+    clinical_events.where(clinical_events.snomedct_code.is_in(referral_codes))
+    .sort_by(clinical_events.date)
+    .first_for_patient()
+    .date
+)
+
+questions[2].check(early_referral)
 # questions[2].hint()
 
 # Question 3
 # Create a boolean patient series indicating whether the date of each patient's earliest diabetes
 # diagnosis was between 1st April 2023 and 31st March 2024. If the patient does not have a
 # diagnosis, the value for in this series should be False.
-questions[3].check(...)
+
+first_diagnosis_date = (
+     clinical_events.where(clinical_events.snomedct_code.is_in(diabetes_codes))
+    .sort_by(clinical_events.date)
+    .first_for_patient()
+    .date
+)
+
+questions[3].check(first_diagnosis_date.is_not_null() & first_diagnosis_date.is_on_or_between("2023-04-01", "2024-03-31"))
 # questions[3].hint()
 
 # Question 4
 # Create a patient series indicating the number of months between a patient's earliest diagnosis
 # and their earliest referral.
-questions[4].check(...)
+month_dig_ref = (early_referral - first_diagnosis_date).months
+
+questions[4].check(month_dig_ref)
 # questions[4].hint()
 
 # Question 5
 # Create a boolean patient series identifying patients who have been diagnosed with diabetes for
 # the first time in the year between 1st April 2023 and 31st March 2024, and who have a record of
 # being referred to a structured education programme within nine months after their diagnosis.
-
-questions[5].check(...)
+questions[5].check(first_diagnosis_date.is_not_null() 
+                   & first_diagnosis_date.is_on_or_between("2023-04-01", "2024-03-31") 
+                   & month_dig_ref.is_not_null() 
+                   & (month_dig_ref <= 9) 
+                   & (month_dig_ref >= 0))
 # questions[5].hint()
 
 # Question 6
 # Create a patient series with the date of the latest record of mild frailty for each patient.
-questions[6].check(...)
+last_mild_frail_date = (
+    clinical_events.where(clinical_events.snomedct_code.is_in(mild_frailty_codes))
+    .sort_by(clinical_events.date)
+    .last_for_patient()
+    .date
+)
+questions[6].check(last_mild_frail_date)
 # questions[6].hint()
 
 # Question 7
 # Create a patient series with the date of the latest record of moderate or severe frailty for
 # each patient.
-questions[7].check(...)
+last_mod_seve_frail_date = (clinical_events
+    .where(clinical_events.snomedct_code.is_in(moderate_frailty_codes)
+          | clinical_events.snomedct_code.is_in(severe_frailty_codes))
+    .sort_by(clinical_events.date)
+    .last_for_patient()
+    .date
+)
+
+questions[7].check(last_mod_seve_frail_date)
 # questions[7].hint()
 
 # Question 8
@@ -83,18 +122,43 @@ questions[7].check(...)
 # Create a boolean patient series indicating whether a patient has moderate or severe frailty, i.e
 # where the patient's last record of severity is moderate or severe. If the patient does not have
 # a record of frailty, the value in this series should be False.
-questions[8].check(...)
+has_frail_date = (clinical_events
+   .where(clinical_events.snomedct_code.is_in(moderate_frailty_codes)
+          | clinical_events.snomedct_code.is_in(severe_frailty_codes)
+          |clinical_events.snomedct_code.is_in(mild_frailty_codes))
+    .sort_by(clinical_events.date)
+    .last_for_patient()
+    .date
+)
+
+questions[8].check(has_frail_date.is_not_null() 
+                   & last_mod_seve_frail_date.is_not_null() 
+                   & (has_frail_date == last_mod_seve_frail_date)
+                   )
 # questions[8].hint()
 
 # Question 9
 # Create a patient series containing the latest HbA1c measurement for each patient.
-questions[9].check(...)
+
+last_hba1c = (
+    clinical_events.where(clinical_events.snomedct_code.is_in(hba1c_codes))
+    .sort_by(clinical_events.date)
+    .last_for_patient()
+    .numeric_value
+)
+
+questions[9].check(last_hba1c)
 # questions[9].hint()
 
 # Question 10
 # Create a boolean patient series identifying patients without moderate or severe frailty in whom
 # the last IFCC-HbA1c is 58 mmol/mol or less
-questions[10].check(...)
+has_mod_sev_frail = (has_frail_date.is_not_null() 
+                   & last_mod_seve_frail_date.is_not_null() 
+                   & (has_frail_date == last_mod_seve_frail_date)
+                   )
+
+questions[10].check(has_mod_sev_frail.is_null() & last_hba1c.is_not_null() & (last_hba1c <= 58))
 # questions[10].hint()
 
 questions.summarise()
